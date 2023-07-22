6fd4d10c5589327be50e09bb801ebd296a443642
Aleks Atanasov <alex2002atanasov@abv.bg>
2023-07-22 14:48:07 +0300

    Complete Day 1

--- Day 01/main.py
+++ Day 01/main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Welcome to the band name generator!")
+city = input("Which city did you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name is " + city + ' ' + pet)
---
+++ Exercises/name_length.py
@@ -0,0 +1,19 @@
+# Print the length of a string
+print(len(input("What is your name?")))
+# --------------------------------------
+
+# Store the input inside a variable
+name = input("What is your name?")
+print(name)
+# ---------------------------------------
+
+# Reverse a & b's values
+a = input("a: ")
+b = input("b: ")
+
+temp = a
+a = b
+b = temp
+
+print("a: " + a)
+print("b: " + b)
