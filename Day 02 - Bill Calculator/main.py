3813762d00e0ff70e4fa064d4a56ce2c7ee90b46
Aleks Atanasov <alex2002atanasov@abv.bg>
2023-07-24 00:05:49 +0300

    Complete Day 2

--- Day 02 - Bill Calculator/main.py
+++ Day 02 - Bill Calculator/main.py
@@ -6,5 +6,5 @@ tip = int(input("How much percentage yip do you want to give 10, 12 or 15? "))
 
 people = int(input("How many people are you? "))
 
-to_pay = float((bill + ((tip / 100) * bill)) / people)
-print(f"$ {round(to_pay, 2)}")
+to_pay_per_person = float((bill + ((tip / 100) * bill)) / people)
+print(f"$ {round(to_pay_per_person, 2)}")
