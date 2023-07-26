43b201d6c365e4915377ea3b527197f6729a83d7
Aleks Atanasov <alex2002atanasov@abv.bg>
2023-07-26 11:53:07 +0300

    Complete Day 7

--- Day 07 - Hangman/day07.py
+++ Day 07 - Hangman/day07.py
@@ -8,23 +8,23 @@ print(chosen_word)
 
 print(hangman_stages.logo)
 
-blanks = []
+display = []
 for letter in chosen_word:
-    blanks += "_"
+    display += "_"
 
 player_lives = 6
 
-while blanks.count("_") != 0 and player_lives != 0:  # or while "_" not in blanks
+while display.count("_") != 0 and player_lives != 0:  # or while "_" not in display
     guessed_letter = input("Guess a letter: ")
 
-    if guessed_letter in blanks:
+    if guessed_letter in display:
         print("You have already guessed that letter")
 
     for position in range(len(chosen_word)):
         if chosen_word[position] == guessed_letter:
-            blanks[position] = guessed_letter
+            display[position] = guessed_letter
 
-    print(blanks)
+    print(display)
     if guessed_letter not in chosen_word:
         print(f"You guessed {guessed_letter}, that's not in the word. You lose a life.")
         print(hangman_stages.stages[player_lives - 1])
@@ -32,7 +32,7 @@ while blanks.count("_") != 0 and player_lives != 0:  # or while "_" not in blank
         if player_lives == 0:
             print("You lose!")
 
-    if "_" not in blanks:
+    if "_" not in display:
         print("You win!")
 
 
