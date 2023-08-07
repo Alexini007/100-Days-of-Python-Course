585b55bd2a7f3783e0ccab8a4929f5ecd341550f
Aleks Atanasov <alex2002atanasov@abv.bg>
2023-08-07 17:29:44 +0300

    Complete Day 21

---
+++ Day 21 - Snake Game/day21.py
@@ -0,0 +1,46 @@
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+import time
+
+my_screen = Screen()
+my_screen.setup(width=600, height=600)
+my_screen.bgcolor("black")
+my_screen.title("Snake Game")
+my_screen.tracer(0)
+
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+
+my_screen.listen()
+my_screen.onkey(snake.up, "Up")
+my_screen.onkey(snake.down, "Down")
+my_screen.onkey(snake.left, "Left")
+my_screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    my_screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    if snake.head.distance(food) < 15:
+        scoreboard.increase_score()
+        food.reposition_food()
+        snake.enlarge_snake()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
+        # creating game border
+        scoreboard.end_of_game()
+        game_is_on = False
+    # detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.end_of_game()
+
+my_screen.exitonclick()
+
---
+++ Day 21 - Snake Game/snake.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.penup()
+        new_segment.color("white")
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def enlarge_snake(self):
+        self.add_segment(self.segments[-1].position())  # add segment to the last
+
+    def up(self):
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+    def down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+    def left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+    def right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            self.segments[seg_num].goto(self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor())
+        self.head.forward(MOVE_DISTANCE)
+
