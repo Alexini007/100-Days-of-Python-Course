da5d9fc2950dca79ba1da0dea625f18a65e157f1
Aleks Atanasov <alex2002atanasov@abv.bg>
2023-09-05 23:05:33 +0300

    Start Day 45

---
+++ Day 45/day45.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+with open("website.html", encoding="utf8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, "html.parser")
+# print(soup.p)
+# print(soup.title.string)
+print(soup.find_all(name="p"))
