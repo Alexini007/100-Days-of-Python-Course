a3e4d9587cabe081f8c7258feb6c0ca271656714
Aleks Atanasov <alex2002atanasov@abv.bg>
2023-09-06 11:05:22 +0300

    Complete Day 45

--- Day 45/day45.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("website.html", encoding="utf8") as file:
-    contents = file.read()
-
-soup = BeautifulSoup(contents, "html.parser")
-# print(soup.p)
-# print(soup.title.string)
-print(soup.find_all(name="p"))
--- Exercises/exercises.py
+++ Exercises/exercises.py
@@ -939,3 +939,57 @@
 #         return "Can drive"
 
 
+# ---------------------------------------
+# Web Scraping with Beautiful Soup
+
+# from bs4 import BeautifulSoup
+
+# with open("website.html", encoding="utf8") as file:
+#     contents = file.read()
+#
+# soup = BeautifulSoup(contents, "html.parser")
+# print(soup.p)
+# print(soup.title.string)
+# all_tags = soup.find_all(name="a")
+#
+# for tag in all_tags:
+#     # print(tag.getText())
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+# heading2 = soup.find(name="h3", class_="heading")
+# print(heading2.get("class"))
+#
+# company_url = soup.select_one(selector="p a")
+# print(company_url)
+#
+# name = soup.select_one(selector="#name")
+# print(name)
+
+# ---------------------------------------
+# Web Scraping with Beautiful Soup
+
+# import requests
+#
+# response = requests.get("https://news.ycombinator.com")
+# yc_webpage = response.text
+# soup = BeautifulSoup(yc_webpage, "html.parser")
+#
+# article_texts = []
+# article_links = []
+#
+# title_line = soup.select(".titleline")
+#
+# for line in title_line:
+#     text = line.find("a").text
+#     article_texts.append(text)
+#     link = line.find("a").get("href")
+#     article_links.append(link)
+#
+# article_upvotes = [int(score.text.split()[0]) for score in soup.select(".score")]
+#
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
+
