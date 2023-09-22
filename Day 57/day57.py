904a98014899a1e1d8e69e7957a58454210b7028
Aleks Atanasov <alex2002atanasov@abv.bg>
2023-09-22 23:45:50 +0300

    Complete Day 57

--- Day 57/day57.py
+++ Day 57/day57.py
@@ -1,10 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
-from flask import render_template
+from flask import Flask, render_template
+import random
+import requests
+import datetime
 
-@app.route("/")
-def hello_home():
-    return 'Hello'
+app = Flask(__name__)
+blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+blog_response = requests.get(blog_url)
+blog_data = blog_response.json()
+
+@app.route('/')
+def home():
+    return render_template("index.html", posts=blog_data)
+
+@app.route('/post/<int:index>')
+def post(index):
+    print(blog_data)
+    return render_template("post.html", posts=blog_data)
 
 
 if __name__ == "__main__":
---
+++ Day 57/post.py
@@ -0,0 +1,3 @@
+class Post:
+    def __init__(self):
+        pass
